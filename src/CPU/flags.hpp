617099d4f9485e59d1978aea6e008222feac4223
nonce <77142078+Existential-nonce@users.noreply.github.com>
2022-02-28 14:18:37 +0000

    changed nullreg to nullflag

--- src/CPU/flags.hpp
+++ src/CPU/flags.hpp
@@ -11,11 +11,11 @@
 struct EFLAGS_REGISTER {
     enum collection { 
         CF,   // carry
-        nullreg = 0,  
+        nullflag = 0,  
         PF,   //parity
-        nullreg = 0,
+        nullflag = 0,
         AF,   // adjust
-        nullreg = 0, 
+        nullflag = 0, 
         ZF,   // zero
         SF,   // sign
         TF,   // trap
@@ -25,15 +25,15 @@ struct EFLAGS_REGISTER {
         IOPL, // I/O priviledge level field
         IOPL, 
         NT,   // nested
-        nullreg = 0,
+        nullflag = 0,
         RF,   // resume
         VM,   // virtual 8086 mode
         AC,   // alignment check
         VIF,  // virtual interrupt
         VIP,  // virtual interrupt pending
         ID,   // identification
-        nullreg = 0, nullreg = 0, nullreg = 0, nullreg = 0, nullreg = 0,  
-        nullreg = 0, nullreg = 0, nullreg = 0, nullreg = 0, nullreg = 0
+        nullflag = 0, nullflag = 0, nullflag = 0, nullflag = 0, nullflag = 0,  
+        nullflag = 0, nullflag = 0, nullflag = 0, nullflag = 0, nullflag = 0
     };
 } FLAGS;
 
